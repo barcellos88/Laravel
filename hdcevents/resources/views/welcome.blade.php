5468ef617c4e1b5f1e9d33aa9381385ebf03ff8e
leonardo <leonardo.barcellos@leolearning.com>
2022-09-17 18:20:49 -0300

    começando os controller's

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -4,31 +4,8 @@
     
 @section('content')
 
-<h1>Algum titulo</h1>
-
-<img src="/img/banner.jpg" alt="Banner">
-
-@if(10 > 15)
-    <p>a condição e true</p>
-@endif
-
-<p>{{ $nome }}</p>
-
-@if($nome == "Pedro")
-    <p> O nome é pedro </p>
-@elseif($nome == "Matheus")
-    <p> O nome é {{ $nome }} e ele tem {{ $idade }} anos e trabalha como {{ $profissao }} </p>
-@else
-    <p>O nome não é Pedro</p>
-@endif
-
-@for($i = 0; $i < count($arr); $i++)
-    <p> {{ $arr[$i] }} - {{ $i }} </p> 
-@endfor
-
-@foreach($nomes as $nomes)
-    <p> {{ $nomes }} </p>
-    <p> {{ $loop->index }}</p>
+@foreach ($events as $events)
+    <p>{{ $events -> title }} -- {{ $events -> description }}</p>
 @endforeach
 
 @endsection
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -26,14 +26,3 @@ Route::get('/events/register', [EventController::class, 'register']);
 Route::get('/contact', function () {
     return view('contact');
 });
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});
