5c001bca07b3d07d4767c5c139a31e740abbbb2e
leonardo <leonardo.barcellos@leolearning.com>
2022-09-17 19:15:57 -0300

    ajuste na tag

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('title', 'HDC Events')
-    
+
 @section('content')
 
 <div id="search-container" class="col-md-12">
@@ -10,21 +10,20 @@
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
-
 <div id="events-container" class="col-md-12">
-    <h2>Próximos eventos</h2>
+    <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
-        @foreach ($events as $event)
-            <div class="card col-md-3">
-                <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
-                <div class="car-body">
-                    <p class="card-date">10/09/2022</p>
-                    <h5 class="card-title">{{ $event->title }}</h5>
-                    <p class="card-participants">X Participantes</p>
-                    <a href="#" class="btn btn-primary">Saber Mais</a>
-                </div>
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
             </div>
+        </div>
         @endforeach
     </div>
 </div>
