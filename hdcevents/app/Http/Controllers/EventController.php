e75ce5039f87820c878a0b5e348a49954a9a90c8
leonardo <leonardo.barcellos@leolearning.com>
2022-09-20 21:41:05 -0300

    add campo data na criação dos eventos e adicionando busca de eventos com validações

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -9,32 +9,42 @@ use App\Models\Event;
 class EventController extends Controller
 {
     
-    public function index(){
+    public function index() {
 
-        $events = Event::all();
+        $search = request('search');
+
+        if($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+            $events = Event::all();
+        }        
     
-        return view('welcome',['events' => $events]);
+        return view('welcome',['events' => $events, 'search' => $search]);
+
     }
 
-    public function create(){
-
+    public function create() {
         return view('events.create');
-
     }
 
-    public function store(Request $request){
+    public function store(Request $request) {
 
         $event = new Event;
 
-        $event->title       = $request->title;
-        $event->city        = $request->city;
-        $event->private     = $request->private;
+        $event->title = $request->title;
+        $event->date = $request->date;
+        $event->city = $request->city;
+        $event->private = $request->private;
         $event->description = $request->description;
-        $event->items       = $request->items;
+        $event->items = $request->items;
 
-        //image Upload
-        if ($request->hasFile('image') && $request->file('image')->isValid()) {
-            
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
             $requestImage = $request->image;
 
             $extension = $requestImage->extension();
@@ -53,26 +63,12 @@ class EventController extends Controller
 
     }
 
-    public function show($id){
+    public function show($id) {
 
         $event = Event::findOrFail($id);
 
         return view('events.show', ['event' => $event]);
-
+        
     }
 
-
-
-    public function login(){
-
-        return view('events.login');
-
-    }
-
-    public function register(){
-
-        return view('events.register');
-
-    }
-    
 }
